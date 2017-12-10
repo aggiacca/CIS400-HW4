e2e2ee288e8bf9d51f5be7666ebabf005cdd6c46
Adam <adamgcog@gmail.com>
2017-12-10 14:59:26 -0500

    Question 3 done

--- Perl-Questions/Question3.pl
+++ Perl-Questions/Question3.pl
@@ -1,25 +1,44 @@
 #!/usr/bin/perl
 
+# Question 3
+# Adam Giaccaglia
+
+
+
 use 5.010;
 use strict;
 use warnings;
 
 my $inputStr = "";
 my @outputMatchs = ("");
+my @matches = ();
+my $outputFilename = 'output.txt';
 
 my $filename = 'input.txt';
 open(my $fh, '<:encoding(UTF-8)', $filename)
   or die "Could not open file '$filename' $!";
  
+ print "Reading File\n";
+ 
 while (my $row = <$fh>) {
   chomp $row;
-  #print "$row\n";
+  if($row =~ /\b([a-z0-9]([a-z0-9.]+[a-z0-9])?\@[a-z0-9.-]+)\b/){
+    push (@matches, $1);
+  }
+
+  
 }
 
-my $testString = "adam\@test.com awfjiojfwej blah\@google.com ";
+print "-------------------------\n";
+print "Done Collecting\n";
 
-my @matches = $testString =~ '^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$';
+my @sortedMatches = sort { lc($a) cmp lc($b) } @matches;
 
-foreach my $match (@matches){
-    print $match;
- }
+
+my $outFile;
+open($outFile, '>', $outputFilename) or die "Could not open file '$outFile' $!";
+foreach my $match (@sortedMatches){
+  print $outFile $match.",\n";
+}
+
+print "Output file complete\n";
